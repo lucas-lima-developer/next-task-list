7cc57ff9a985fecd693904dc725ce8106797c462
Lucas Lima <lucasViniLima15@gmail.com>
2024-03-19 16:43:59 -0300

    feat: adicionado nome de propiedades no form e link para login

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,24 +1,32 @@
-import { createUser } from "@/lib/actions";
+import { signupUser } from "@/lib/actions";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <div className={styles.container}>
-        <form className={styles["login-box"]} action={createUser}>
+        <form className={styles["login-box"]} action={signupUser}>
           <h1>Signup</h1>
           <div className={styles["login-input"]}>
-            <input type="text" placeholder="E-mail" />
+            <input type="text" placeholder="E-mail" name="email" />
           </div>
           <div className={styles["login-input"]}>
-            <input type="password" placeholder="Senha" />
+            <input type="password" placeholder="Senha" name="senha" />
           </div>
           <div className={styles["login-input"]}>
-            <input type="password" placeholder="Confirmar senha" />
+            <input
+              type="password"
+              placeholder="Confirmar senha"
+              name="confirmarSenha"
+            />
           </div>
           <div className={styles["login-button"]}>
             <button type="submit">Cadastrar</button>
           </div>
+          <p>
+            Já possui conta? Faça o <Link href="/login">Login</Link>
+          </p>
         </form>
       </div>
     </main>
