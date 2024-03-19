db262510854f97e7b6579321fa45da632637346c
Lucas Lima <lucasViniLima15@gmail.com>
2024-03-19 16:44:25 -0300

    feat: adiconado a lógica de cadastrar o usuário no bd

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,16 +2,47 @@
 
 import User from "@/models/User"
 import dbConnect from "./dbConnect"
+import { redirect } from "next/navigation";
+import bcrypt from 'bcryptjs';
 
-export async function createUser() {
+async function findUserByEmail(email: any) {
 	await dbConnect();
 
-	const user = await User.create({
-		email: 'lucas',
-		senha: '1234'
-	});
+	const user = await User.findOne({ email }).lean();
 
-	console.log(user);
-
-	return user
+	return user;
 }
+
+export async function signupUser(formData: FormData) {
+	const email= formData.get('email')?.toString();
+	const senha = formData.get('senha')?.toString();
+	const confirmarSenha = formData.get('confirmarSenha')?.toString();
+
+	try {
+		if (!email || !senha || !confirmarSenha || senha !== confirmarSenha) {
+			throw new Error('Por favor, forneça um email válido e as senhas devem ser iguais');
+		}
+
+		const hashedPassword = await bcrypt.hash(senha, 10);
+
+		const user = await findUserByEmail(email);
+
+		if (user) throw new Error("Esse e-mail já está cadastrado!")
+
+		await dbConnect();
+
+		const newUser = await User.create({
+			email,
+			senha: hashedPassword
+		});
+
+		if (!newUser) throw new Error("Alguma coisa deu errado, entre em contato com o suporte.");
+	} catch (error: any) {
+		console.log(error.message);
+		return 
+	}
+
+	redirect('/login');
+}
+
+// TODO Criar Lógica do login, mandando para a '/' quando for autenticado
