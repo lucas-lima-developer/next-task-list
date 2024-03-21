ca79d59e92785f02e75fa5c38db345c9b36aa89a
Lucas Lima <lucasViniLima15@gmail.com>
2024-03-21 19:11:46 -0300

    feat: utilizando useFormState para "pegar" os erros

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { signupUser } from "@/lib/actions";
 import styles from "./page.module.css";
-import Link from "next/link";
+import Link from "next/link"; 
+import { useFormState } from "react-dom";
 
 export default function Home() {
+  // TODO Mostrar mensagens de erro do Next.js
+  const [state, formAction] = useFormState(signupUser, null);
+
   return (
     <main>
       <div className={styles.container}>
-        <form className={styles["login-box"]} action={signupUser}>
+        <form className={styles["login-box"]} action={formAction}>
           <h1>Signup</h1>
           <div className={styles["login-input"]}>
             <input type="text" placeholder="E-mail" name="email" />
@@ -21,6 +27,8 @@ export default function Home() {
               name="confirmarSenha"
             />
           </div>
+          {/* TODO Adicionar esse estilo no module.css da pagina */}
+          <p style={{fontSize: 14, color: 'red', marginTop: -10}}>{state}</p>
           <div className={styles["login-button"]}>
             <button type="submit">Cadastrar</button>
           </div>
