b0b108395204f1da4cb0dd08bb86e166d781722d
Lucas Lima <lucasViniLima15@gmail.com>
2024-03-21 19:12:46 -0300

    feat: adicionando o Schema do FormData

---
+++ src/lib/schema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const FormDataSchema = z.object({
+  email: z.string().email({ message: 'O email é necessário' }),
+  senha: z.string().min(5, { message:  'A senha deve ter mais de 5 caracteres' }),
+  confirmarSenha: z.string()
+});
