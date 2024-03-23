3c5c723020b1fa88a028b1efc07405bd0bb55a9f
Lucas Lima <lucasViniLima15@gmail.com>
2024-03-22 22:50:53 -0300

    feat: adicionando TODOs

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      {/* TODO Página onde deve ficar o Dahsboard com as tarefas */}
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Página principal</h1>
+      {/* TODO fazer a página explicando como funciona o projeto */}
+      <h1>Página explicando o projeto</h1>
     </main>
   );
 }
