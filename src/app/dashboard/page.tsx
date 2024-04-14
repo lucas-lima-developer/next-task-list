eac255bc4657d22b788832a52e64c7b1dda7e4c0
Lucas Lima <lucasViniLima15@gmail.com>
2024-04-13 23:42:16 -0300

    fix: adicionando estilização e componentes do dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,34 @@
 import { getEmailFromToken } from "@/lib/authServices";
 import { redirect } from "next/navigation";
+import styles from "./page.module.css";
+import FormCriarTarefa from "./FormCriarTarefa";
+import { findUserByEmail } from "@/lib/userServices";
+import { getAllTasksFromUser } from "@/lib/taskServices";
+import ListaTarefas from "./ListaTarefas";
 
 export default async function Home() {
   const email = await getEmailFromToken();
-
-  if (!email) {
-    redirect('/login')
+  const user = await findUserByEmail(email);
+  
+  if (!email || !user) {
+    redirect("/login");
   }
 
+  const tasks = await getAllTasksFromUser(user);
+
+  console.log(tasks);
+
   return (
-    <main>
-      {/* TODO Página onde deve ficar o Dahsboard com as tarefas */}
-      <h1>Dashboard</h1>
-      <p>{email}</p>
-    </main>
+    <>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <h1>Dashboard</h1>
+        </header>
+        <main className={styles.main}>
+          <FormCriarTarefa />
+          <ListaTarefas tasks={tasks}/>
+        </main>
+      </div>
+    </>
   );
 }
