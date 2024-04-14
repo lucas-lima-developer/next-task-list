ca09cb579a55c4cdc3ee51010bff5a7b412b2ed6
Lucas Lima <lucasViniLima15@gmail.com>
2024-04-13 23:37:23 -0300

    fix: adicionando retorno da função de achar usuário pelo email

--- src/lib/userServices.ts
+++ src/lib/userServices.ts
@@ -1,12 +1,19 @@
 import User from "@/models/User";
 import dbConnect from "./dbConnect";
+import { Schema } from "mongoose";
 
-export async function findUserByEmail(email: any) {
+export interface UserDocument{
+  _id: Schema.Types.ObjectId;
+  email: string;
+  senha: string;
+}
+
+export async function findUserByEmail(email: any) : Promise<UserDocument | null> {
 	await dbConnect();
 
 	const user = await User.findOne({ email }).lean();
 
-	return user;
+	return user as UserDocument;
 }
 
 export async function createUser(user: { email: string, senha: string }) {
