812fbc9aee92e02a1aa358de725e6cf15675771e
Lucas Lima <lucasViniLima15@gmail.com>
2024-04-17 11:56:45 -0300

    feat: adicionando componente de tarefa

--- src/app/dashboard/ListaTarefas.tsx
+++ src/app/dashboard/ListaTarefas.tsx
@@ -1,16 +1,39 @@
-import { TaskArray } from "@/models/Task";
+"use client";
+
+import { deleteTaskAction } from "@/lib/actions";
 import styles from "./page.module.css";
 
-export default function ListaTarefas({ tasks }: { tasks: TaskArray }) {
+export type TaskProps = {
+  title: string;
+  isComplete: boolean;
+  _id: string;
+  createdAt: Date;
+  user: string;
+};
+
+type Props = {
+  tasks: TaskProps[];
+};
+
+export default function ListaTarefas({ tasks }: Props) {
   return (
-    <>
-      <ul className={styles["task-list"]}>
-            {tasks.map((task) => (
-              <li key={task._id} className={styles["task-item"]}>
-                {task.title}
-              </li>
-            ))}
-          </ul>
-    </>
-  )
+    <ul className={styles["task-list"]}>
+      {tasks.map((task) => (
+        <TaskComponent task={task} key={task._id} />
+      ))}
+    </ul>
+  );
+}
+
+function TaskComponent({ task }: { task: TaskProps }) {
+  const deleteTaskWithId = deleteTaskAction.bind(null, task._id);
+
+  return (
+    <li className={styles["task-item"]}>
+      <span>{task.title}</span>
+      <form action={deleteTaskWithId}>
+        <button className={styles["task-button"]}>X</button>
+      </form>
+    </li>
+  );
 }
