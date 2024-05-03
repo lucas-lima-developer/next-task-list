485351a69453079c31d1bc04a855850c7339117e
Lucas Lima <lucasViniLima15@gmail.com>
2024-05-03 10:16:55 -0300

    fix: alterando path dos documentos

--- src/lib/models/Task.ts
+++ src/lib/models/Task.ts
@@ -1,10 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
+// TODO fazer um discovery para ver se vale a pena mudar isso para uma pasta
 export interface Task extends mongoose.Document {
   title: string;
   isComplete: boolean;
   createdAt: Date;
   user: mongoose.Types.ObjectId;
+  _id: mongoose.Types.ObjectId;
 }
 
 export type TaskArray = Task[];
