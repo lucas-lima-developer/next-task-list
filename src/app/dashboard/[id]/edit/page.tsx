c08e47de5fcfdb3214a5838cfc67521f998c61df
Lucas Lima <lucasViniLima15@gmail.com>
2024-06-27 12:42:02 -0300

    fix: adicionando componente do formulário de atualizar tarefa

--- src/app/dashboard/[id]/edit/page.tsx
+++ src/app/dashboard/[id]/edit/page.tsx
@@ -1,39 +1,20 @@
 import { updateTaskAction } from "@/lib/actions";
 import { redirect } from "next/navigation";
-import styles from "@/app/dashboard/page.module.css";
 import Link from "next/link";
 import TaskService from "@/lib/services/TaskService";
+import FormEditTask from "@/components/dashboard/edit/FormEditTask";
 
-export default async function Home({ params }: { params: { id: string } }) {
-  const id = params.id;
-
-  const task = JSON.parse(JSON.stringify(await TaskService.get(id)))
+export default async function EditTaskPage({ params }: { params: { id: string } }) {
+  const task = JSON.parse(JSON.stringify(await TaskService.get(params.id)))
   const title = task.title;
 
   const updateTaskWithId = updateTaskAction.bind(null, task);
   
-
   if (!task) redirect("/dashboard");
 
   return (
-    <main className={styles.main}>
-      <form className="form" action={updateTaskWithId}>
-        <input
-          type="text"
-          className={styles["text-input"]}
-          placeholder="Nova tarefa"
-          name="title"
-          defaultValue={title}
-        />
-        <div className={styles["div-centralizada"]}>
-          <button type="submit" className={styles["submit-button"]}>
-            Atualizar
-          </button>
-          <button className={styles["back-button"]}>
-            <Link href={`/dashboard/`}>Voltar</Link>
-          </button>
-        </div>
-      </form>
+    <main>
+      <FormEditTask task={task}/>
     </main>
   );
 }
