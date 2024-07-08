9a980d187a3faae72ea1c8a0c7e22a50b8a06b26
Lucas Lima <lucasViniLima15@gmail.com>
2024-07-08 10:45:30 -0300

    feat: adicionando design da página de Profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,12 +1,20 @@
+"use client";
+
 import Link from "next/link";
 
-interface ProfileProps {
-  email: string,
-  allTasks: number,
-  allCompletedTasks: number
-};
+import styles from "@/components/profile/Profile.module.css";
 
-export default function Profile({ email, allTasks, allCompletedTasks }: ProfileProps) {
+interface ProfileProps {
+  email: string;
+  allTasks: number;
+  allCompletedTasks: number;
+}
+
+export default function Profile({
+  email,
+  allTasks,
+  allCompletedTasks,
+}: ProfileProps) {
   // Email  do usuário (feito)
   // Quantidade de tarefas produzidas (feito)
   // Quantidade de tarefas concluídas (feito)
@@ -14,13 +22,25 @@ export default function Profile({ email, allTasks, allCompletedTasks }: ProfileP
   // Botão mudar senha -> Página que muda senha
   // Botão apagar conta -> Pop up que pede a senha atual para deletar a conta
   return (
-    <>
-      <p>E-mail: {email}</p>
-      <p>Número total de tarefas: {allTasks}</p>
-      <p>Número total de tarefas concluídas: {allCompletedTasks}</p>
-      <button><Link href={`/profile/email`}>Mudar email</Link></button>
-      <button><Link href={`profile/senha`}>Mudar senha</Link></button>
-      <button>Apagar conta</button>
-    </>
+    <div className={styles.box}>
+      <div>
+        <p>
+          <b>Email:</b> {email}
+        </p>
+        <p>
+          <b>Número total de tarefas:</b> {allTasks}
+        </p>
+        <p>
+          <b>Número total de tarefas concluídas:</b> {allCompletedTasks}
+        </p>
+        <Link href={`/profile/email`}>
+          <button>Mudar email</button>
+        </Link>
+        <Link href={`profile/senha`}>
+          <button>Mudar senha</button>
+        </Link>
+        <button>Apagar conta</button>
+      </div>
+    </div>
   );
 }
